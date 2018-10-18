ec2a3628ebf51b52f568d4f003daee935ac500ce
jacob <jacob@jacobs-MacBook-Air.local>
2018-10-18 00:10:39 -0400

    edited pic scriptmysql

--- hamburgerapp_sql_scripts/pic_script.sql
+++ hamburgerapp_sql_scripts/pic_script.sql
@@ -4,6 +4,8 @@ CREATE TABLE  `picture` (
   `user_id` int(11) NOT NULL,
   `pic_url` varchar(255) NOT NULL,
   `pic_name` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+      `brand` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
     CONSTRAINT `picture` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ;
